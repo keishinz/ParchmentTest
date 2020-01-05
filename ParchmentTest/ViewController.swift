da6c6d835dc0cf749b27cb445ec036ca9a319cab
Keishin CHOU <keishin.chou@gmail.com>
2020-01-05 14:42:17 +0900

    Icon CollectionView test.

--- ParchmentTest/ViewController.swift
+++ ParchmentTest/ViewController.swift
@@ -10,102 +10,123 @@ import UIKit
 
 import Parchment
 
+struct IconItem: PagingItem, Hashable, Comparable {
+    
+    let icon: String
+    let index: Int
+    let image: UIImage?
+    
+    init(icon: String, index: Int) {
+        self.icon = icon
+        self.index = index
+        self.image = UIImage(named: icon)
+    }
+    
+    var hashValue: Int {
+        return icon.hashValue
+    }
+        
+    static func <(lhs: IconItem, rhs: IconItem) -> Bool {
+        return lhs.index < rhs.index
+    }
+    
+    static func ==(lhs: IconItem, rhs: IconItem) -> Bool {
+        return (
+            lhs.index == rhs.index &&
+                lhs.icon == rhs.icon
+        )
+    }
+}
+
 class ViewController: UIViewController {
     
-    fileprivate let cities = [
-      "Oslo",
-      "Stockholm",
-      "Tokyo",
-      "Barcelona",
-      "Vancouver",
-      "Berlin",
-      "Shanghai",
-      "London",
-      "Paris",
-      "Chicago",
-      "Madrid",
-      "Munich",
-      "Toronto",
-      "Sydney",
-      "Melbourne"
+    fileprivate let icons = [
+      "compass",
+      "cloud",
+      "bonnet",
+      "axe",
+      "earth",
+      "knife",
+      "leave",
+      "light",
+      "map",
+      "moon",
+      "mushroom",
+      "shoes",
+      "snow",
+      "star",
+      "star-1",
+      "sun",
+      "tipi",
+      "tree",
+      "water",
+      "wind",
+      "wood"
     ]
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let pagingViewController = PagingViewController<PagingIndexItem>()
+//        let pagingViewController = PagingViewController<PagingIndexItem>()
+//        pagingViewController.dataSource = self
+//        pagingViewController.delegate = self
+        let pagingViewController = PagingViewController<IconItem>()
+        pagingViewController.menuItemSource = .class(type: IconPagingCell.self)
+        pagingViewController.menuItemSize = .fixed(width: 60, height: 60)
+        pagingViewController.textColor = UIColor(red: 0.51, green: 0.54, blue: 0.56, alpha: 1)
+        pagingViewController.selectedTextColor = UIColor(red: 0.14, green: 0.77, blue: 0.85, alpha: 1)
+        pagingViewController.indicatorColor = UIColor(red: 0.14, green: 0.77, blue: 0.85, alpha: 1)
         pagingViewController.dataSource = self
-        pagingViewController.delegate = self
+        pagingViewController.select(pagingItem: IconItem(icon: icons[0], index: 0))
         
-//        let view1 = ViewControllerOne(index: 1)
-//        let view2 = ViewControllerOne(index: 2)
-//        let view3 = ViewControllerOne(index: 3)
-//        let view4 = ViewControllerOne(index: 2)
-//        let view5 = ViewControllerOne(index: 3)
-//
-//        let pagingViewController = FixedPagingViewController(viewControllers: [
-//          view1,
-//          view2,
-//          view3,
-//          view4,
-//          view5,
-//        ])
         addChild(pagingViewController)
         view.addSubview(pagingViewController.view)
         view.constrainToEdges(pagingViewController.view)
         pagingViewController.didMove(toParent: self)
-//        pagingViewController.view.translatesAutoresizingMaskIntoConstraints = false
-        
-//        NSLayoutConstraint.activate([
-//          pagingViewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-//          pagingViewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-//          pagingViewController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-//          pagingViewController.view.topAnchor.constraint(equalTo: view.topAnchor)
-//        ])
         
     }
-
-
 }
 
 extension ViewController: PagingViewControllerDataSource {
     func numberOfViewControllers<T>(in pagingViewController: PagingViewController<T>) -> Int where T : PagingItem, T : Comparable, T : Hashable {
-        return cities.count
+//        return cities.count
+        return icons.count
     }
     
     func pagingViewController<T>(_ pagingViewController: PagingViewController<T>, viewControllerForIndex index: Int) -> UIViewController where T : PagingItem, T : Comparable, T : Hashable {
-        return CityViewController(title: cities[index])
+//        return CityViewController(title: cities[index])
+        return IconViewController(title: icons[index].capitalized)
     }
     
     func pagingViewController<T>(_ pagingViewController: PagingViewController<T>, pagingItemForIndex index: Int) -> T where T : PagingItem, T : Comparable, T : Hashable {
-        return PagingIndexItem(index: index, title: cities[index]) as! T
-    }
-    
-    
-}
-
-extension ViewController: PagingViewControllerDelegate {
-    func pagingViewController<T>(_ pagingViewController: PagingViewController<T>, widthForPagingItem pagingItem: T, isSelected: Bool) -> CGFloat? where T : PagingItem, T : Comparable, T : Hashable {
-        
-        guard let item = pagingItem as? PagingIndexItem else { return 0 }
-        
-        let insets = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
-        let size = CGSize(width: CGFloat.greatestFiniteMagnitude, height: pagingViewController.menuItemSize.height)
-        let attributes = [NSAttributedString.Key.font: pagingViewController.font]
-        
-        let rect = item.title.boundingRect(with: size,
-          options: .usesLineFragmentOrigin,
-          attributes: attributes,
-          context: nil)
-
-        let width = ceil(rect.width) + insets.left + insets.right
-        
-        if isSelected {
-          return width * 1.5
-        } else {
-          return width
-        }
+//        return PagingIndexItem(index: index, title: cities[index]) as! T
+        return IconItem(icon: icons[index], index: index) as! T
     }
 }
+
+//extension ViewController: PagingViewControllerDelegate {
+//    func pagingViewController<T>(_ pagingViewController: PagingViewController<T>, widthForPagingItem pagingItem: T, isSelected: Bool) -> CGFloat? where T : PagingItem, T : Comparable, T : Hashable {
+//
+//        guard let item = pagingItem as? PagingIndexItem else { return 0 }
+//
+//        let insets = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+//        let size = CGSize(width: CGFloat.greatestFiniteMagnitude, height: pagingViewController.menuItemSize.height)
+//        let attributes = [NSAttributedString.Key.font: pagingViewController.font]
+//
+//        let rect = item.title.boundingRect(with: size,
+//          options: .usesLineFragmentOrigin,
+//          attributes: attributes,
+//          context: nil)
+//
+//        let width = ceil(rect.width) + insets.left + insets.right
+//
+//        if isSelected {
+//          return width * 1.5
+//        } else {
+//          return width
+//        }
+//    }
+//}
 
